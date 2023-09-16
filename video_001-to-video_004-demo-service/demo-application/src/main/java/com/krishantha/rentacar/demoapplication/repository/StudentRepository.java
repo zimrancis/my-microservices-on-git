6594e011464b8a8451c2750168bf34a1c8766394
zimrancis <zimrancis@gmail.com>
2023-09-17 05:56:06 +0600

    Delete video_001-to-video_004-demo-service directory

--- video_001-to-video_004-demo-service/demo-application/src/main/java/com/krishantha/rentacar/demoapplication/repository/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.krishantha.rentacar.demoapplication.repository;
-
-import com.krishantha.rentacar.demoapplication.model.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.Repository;
-
-public interface StudentRepository extends JpaRepository<Student,Integer> {
-
-}
